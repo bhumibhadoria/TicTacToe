8356e742823afefca052a00f960e4eb7d176a9aa
BhumiBhadoria <113108322+BhumiBhadoria@users.noreply.github.com>
2023-05-12 01:18:48 +0530

    Some new files added and some updations in code.

---
+++ ttt.js
@@ -0,0 +1,18 @@
+console.log("Welcome to tic tac toe")
+let music = new Audio("music.mp3")
+let turn = new Audio("turn.wav")
+let gameover = new Audio("gameover.wav")
+let turn = "X"
+
+// function to change the turn
+const changeTurn = () => {
+    return turn === "X"?"0" : "X"
+}
+ 
+// function to check for a Win
+const checkWin = () => {
+
+}
+
+// Game logic
+
